a64ce0b06014465de89e9c2a7dcae24ae32fcded
Junaid Atari <junaid@openletterconnect.com>
2025-01-31 16:04:55 +0500

    change app name and script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
+const config = require('package.json');
+
 /**
  * @see https://pm2.keymetrics.io/docs/usage/application-declaration/
  */
 module.exports = {
   apps: [
     {
-      name: 'node-ts-boilerplate',
-      script: 'dist/index.js',
+      name: config.name,
+      script: config.main,
       //watch: true,
       //instances: 4,
       //exec_mode: 'cluster',
